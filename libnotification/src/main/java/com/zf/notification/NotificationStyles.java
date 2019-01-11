d37d8d85551f863ad36cec963fa0701b45ce2952
wangxiaotong <wang.xiaotong05382@westone.com.cn>
2019-01-11 20:13:10 +0800

    优化通知栏显示适配

--- libnotification/src/main/java/com/zf/notification/NotificationStyles.java
+++ libnotification/src/main/java/com/zf/notification/NotificationStyles.java
@@ -1,5 +1,6 @@
 package com.zf.notification;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.View;
 import android.widget.RemoteViews;
@@ -28,9 +29,22 @@ public class NotificationStyles {
      * @param largeIcon 图标
      * @return
      */
+    @SuppressLint("NewApi")
     public RemoteViews setCustomRemoteViews(String title, String content, String intent, int largeIcon) {
         //自定义布局
         RemoteViews remoteView = new RemoteViews(mContext.getPackageName(), R.layout.notification_layout);
+        //判断传递值为空情况
+        if (title == null || "".equals(title)){
+            remoteView.setViewVisibility(R.id.text1,View.GONE);
+            remoteView.setTextColor(R.id.text1,mContext.getColor(R.color.black));
+        }
+        if (content == null || "".equals(content)){
+            remoteView.setViewVisibility(R.id.text2,View.GONE);
+            remoteView.setTextColor(R.id.text2,mContext.getColor(R.color.black));
+        }
+        if (intent == null || "".equals(intent)) {
+            remoteView.setViewVisibility(R.id.tvIntent, View.GONE);
+        }
         /**大图标*/
         remoteView.setImageViewResource(R.id.imgNotifition, largeIcon);
         /**标题*/
@@ -38,12 +52,7 @@ public class NotificationStyles {
         /**内容*/
         remoteView.setTextViewText(R.id.text2, content);
         /**设置等按钮*/
-        remoteView.setViewVisibility(R.id.flButton, View.GONE);
-        if (intent != null) {
-            remoteView.setViewVisibility(R.id.flButton, View.VISIBLE);
-            remoteView.setImageViewResource(R.id.imgIntent, R.drawable.btn_notification);
-            remoteView.setTextViewText(R.id.tvIntent, intent);
-        }
+        remoteView.setTextViewText(R.id.tvIntent, intent);
         return remoteView;
     }
 }
